51f8a4f094b275563edc90767ae115d4265f496b
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-11-24 15:31:42 -0400

    documentando tokens

---
+++ packages/docs/storybook-static/assets/preview.f8859a69.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.c6a19b01.js";import"./iframe.30f7fb8d.js";import"./es.map.constructor.59696c85.js";import"./es.number.to-fixed.c83d604b.js";import"./index.6b9cb52a.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.f8859a69.js.map
